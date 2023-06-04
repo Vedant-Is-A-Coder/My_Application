509a54ff7a858a5e5e58e4421710f021e5e72cc6
Vedant-Is-A-Coder <vedantabhyankar.10@gmail.com>
2023-06-04 11:57:46 +0530

    yo

---
+++ MyApplication7/app/src/main/java/com/example/myapplication/customdatatype.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class customdatatype {
+    public String question;
+    public String answer;
+
+    public customdatatype(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+
+    }
+}
